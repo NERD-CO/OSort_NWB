49a3be63b38045eb59972fc8635855cc52a73240
John Thompson <john.a.thompson@cuanschutz.edu>
2022-03-02 14:46:17 -0700

    update

--- RUN_OSORT_NWB.m
+++ RUN_OSORT_NWB.m
@@ -1,16 +1,16 @@
 %% TO RUN OSORT-NWB
 
 
-%% Step 1a Make sure matnwb is ON YOUR PATH
+%% Step 1a Make sure matnwb is ON YOUR PATH (all sub-folders)
 
-%% Step 1b Make sure seegBRAINplot_s is ON YOUR PATH
+%% Step 1b Make sure OSort_NWB is ON YOUR PATH (Top Folder)
 
-%% Step 1c Make sure seegBRAINplot_s/osort-v4-rel is ON YOUR PATH (all sub-folders)
+%% Step 1c Make sure OSort_NWB/osort-v4-rel is ON YOUR PATH (all sub-folders)
 
 %% Step 2 Create a new folder for spike sorting a session
 % i.e., C:/Documents/Data/Study1/SpikeSorting/Session1
 
-%% Step 3 Inside folder NEW Create an 'nwb' folder
+%% Step 3 Inside NEW folder create an 'nwb' folder
 % i.e., C:/Documents/Dat/Study1/SpikeSorting/Session1/nwb
 
 %% Step 4 Copy and past the NWB file of interest and '+types' folder from NWB repo folder
@@ -23,7 +23,7 @@
 %% Step 5 Change the following input arguments:
 % basepath 
 % -- This should be the outer folder you created in STEP 2
-basepath = 'C:\Users\Admin\Desktop\testfold\';
+basepath = 'C:/Documents/Data/Study1/SpikeSorting/Session1\';
 patientID = 'MW_1';
 nwbFname = 'MW1_Session_2_raw.nwb';
 
