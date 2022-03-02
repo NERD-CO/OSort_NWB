7b40855b2b8bf0de218b518fd231b440878ae9cb
John Thompson <john.a.thompson@cuanschutz.edu>
2022-03-02 14:44:13 -0700

    first commit

---
+++ ProcessSessionNCS_css.m
@@ -0,0 +1,23 @@
+function [] = ProcessSessionNCS_css(folderLOC)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+cd(folderLOC)
+
+% get list of ncs
+
+
+% get unique number of underscores
+
+
+% get outer folder
+
+
+% create new RecBlok folders - move and rename
+
+
+
+
+
+
+end
